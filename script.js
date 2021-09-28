0fbf9fc48d96991434d77a8fd343cc165f4ecf7b
Leslie A. Hurtado <lesliehurtado1103@gmail.com>
2021-09-28 11:49:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,32 @@ $( document ).ready(function() {
         $("#authdesc").toggleClass("hidden");
         $("#webdesc").toggleClass("hidden");
 });
-		$("#series1").click(function(){
+		$("#series2").click(function(){
     	$("#multimodal").toggleClass("hidden");
        if ($("#multimodal").hasClass("hidden")) {
     	$("#multimodallit").toggleClass("hidden");
+      $("#endofseriesreflec2").toggleClass("hidden");
     }
     });
     $("#mml").click(function(){
     	$("#multimodallit").toggleClass("hidden");
+      if ($("#endofseriesreflec2").hasClass("hidden")) {
+      	console.log("hidden");
+      }
+      else {
+      $("#endofseriesreflec2").toggleClass("hidden");
+      }
     });
+    $("#eosr2").click(function(){
+    	$("#endofseriesreflec2").toggleClass("hidden");
+      if ($("#multimodallit").hasClass("hidden")) {
+      	console.log("hidden");
+      }
+      else {
+      $("#multimodallit").toggleClass("hidden");
+      }
+    });
+    if ($("#multimodal").hasClass("hidden")) {
+    	$("#multimodallit").toggleClass("hidden"); 
+      $("#endofseriesreflec2").toggleClass("hidden");}
 });
